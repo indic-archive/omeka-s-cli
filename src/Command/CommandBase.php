fa03f5c4f67b8b6f9fcf65a3cdd8b4cc46c66cfc
Junaid PV <junu.pv@gmail.com>
2024-08-10 14:55:00 +0530

    Parent for all comand with support for configuration.

---
+++ src/Command/CommandBase.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace IDAF\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use IDAF\IDAFOutputStyle;
+
+abstract class CommandBase extends Command {
+
+    /**
+     * Styled output helper.
+     *
+     * @var \IDAF\IDAFOutputStyle
+     */
+    protected $styledOutput;
+
+    public function getConfig(): array {
+        $config = [];
+
+        $config_file_path = $_SERVER['HOME'] . '/.omeka-s-cli.yml';
+        if (file_exists($config_file_path)) {
+            $config = Yaml::parseFile($config_file_path);
+        }
+
+        return $config;
+    }
+
+    protected function configure(): void {
+
+        $config = $this->getConfig();
+
+        if (isset($config['site-dir'])) {
+            $default_root_dir = $config['site-dir'];
+        }
+        else {
+            $default_root_dir = getcwd();
+        }
+
+        $this
+            ->addOption('site-dir', 'r', InputOption::VALUE_REQUIRED, 'Omeka S site directory.', $default_root_dir)
+        ;
+    }
+
+    /**
+     * Get styled output helper.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     *  Input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *  Output
+     * @return \WWMApp\WWMOutputStyle
+     */
+    protected function getStyledOutput(InputInterface $input, OutputInterface $output) {
+        if (!$this->styledOutput) {
+            $this->styledOutput = new IDAFOutputStyle($input, $output);
+        }
+        return $this->styledOutput;
+    }
+}
